c6645c6fbb9bff6ce477faa5c73d21a93cd1f6b1
suryateja-challa <challasuryateja@gmail.com>
2023-03-24 09:43:10 +0530

    Adding modified dataset

--- Classifier.py
+++ Classifier.py
@@ -1,1 +1,12 @@
+import tensorflow as tf
 import os
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+import cv2
+import imghdr
+
+data_dir = 'data'
+image_extension = ['jpg', 'jpeg', 'png', 'bmp']
